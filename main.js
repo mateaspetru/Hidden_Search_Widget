c589d85b20b919771467a33f7dc160c9e1a91a0a
Mateas Petru <mateaspetru@gmail.com>
2023-08-09 20:01:02 +0300

    toggle active class

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-import "./style.css";
+const $ = (selector) => document.querySelector(selector);
+const container = $(".container");
+const input = $(".input");
+const btn = $(".btn");
+
+btn.addEventListener("click", () => {
+  container.classList.toggle("active");
+  input.classList.toggle("active");
+});
